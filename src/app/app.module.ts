e0ef3238c5294234b564a67052004820e8918c42
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-09-21 15:30:39 +0200

    create overrview component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,13 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 import { HomeComponent } from './components/home/home.component';
 import { SidenavComponent } from './components/home/sidenav/sidenav.component';
+import { NavComponent } from './components/home/nav/nav.component';
+import { OverviewComponent } from './components/home/overview/overview.component';
 
 const materialModules = [
 	MatCardModule,
@@ -24,10 +29,20 @@ const materialModules = [
 	MatButtonModule,
 	MatIconModule,
 	MatToolbarModule,
+	MatSidenavModule,
+	MatGridListModule,
 ];
 
 @NgModule({
-	declarations: [AppComponent, LoginComponent, RegisterComponent, HomeComponent, SidenavComponent],
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		RegisterComponent,
+		HomeComponent,
+		SidenavComponent,
+  NavComponent,
+  OverviewComponent,
+	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
