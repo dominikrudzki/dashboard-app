d85270bd09ac9f067baef0a67c9b0660e04fd773
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-09-23 23:55:17 +0200

    add form submit handler

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
 	selector: 'app-login',
@@ -6,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-	constructor() {}
+	loginForm!: FormGroup;
 
-	ngOnInit(): void {}
+	constructor(
+		private DataService: DataService,
+		private formBuilder: FormBuilder
+	) {}
+
+	ngOnInit(): void {
+		this.reactiveForm();
+	}
+
+	reactiveForm() {
+		this.loginForm = this.formBuilder.group({
+			username: [''],
+			password: [''],
+		});
+	}
+
+	submitForm() {
+		this.DataService.logIn(this.loginForm.value);
+	}
 }
