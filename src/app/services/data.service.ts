e879b1bab39cc080f0b4ef8f65225c31b55840f5
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-09-24 11:21:55 +0200

    add userData setter in data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,27 +1,18 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class DataService {
-	userData: any;
+	private userData: any;
 
-	constructor(private firestore: AngularFirestore) {}
+	constructor() {}
 
-	logIn(value: { username: string; password: string }) {
-		console.log('value', value);
-
-		this.firestore
-			.collection('users')
-			.doc(`${value.username}`)
-			.valueChanges()
-			.subscribe((data: { password: string } | any) => {
-				if ((data.password = value.password)) {
-					alert('you are logged in');
-				}
-			});
+	setUserData(userData: any) {
+		this.userData = userData;
 	}
+
+	logIn(value: { username: string; password: string }) {}
 
 	register(value: { username: string; password: string }) {}
 }
