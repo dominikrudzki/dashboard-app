bb3066f5c92c3cd8afc4f3d450a1c423e3a6b4f7
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-09-24 12:42:19 +0200

    add logout route

--- src/app/components/home/sidenav/sidenav.component.ts
+++ src/app/components/home/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'src/app/services/cookie.service';
 
 @Component({
 	selector: 'app-sidenav',
@@ -6,19 +7,23 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
-	navItems: { icon: string; text: string; route: string; styles?: string }[] =
-		[
-			{ icon: 'home', text: 'Overview', route: 'overview' },
-			{ icon: 'folder', text: 'Projects', route: 'projects' },
-			{ icon: 'chat', text: 'Chat', route: 'chat' },
-			{
-				icon: 'settings',
-				text: 'Settings',
-				route: 'settings',
-				styles: 'auto',
-			},
-			{ icon: 'logout', text: 'Log out', route: '' },
-		];
+	navItems: {
+		icon: string;
+		text: string;
+		route: string;
+		styles?: string;
+	}[] = [
+		{ icon: 'home', text: 'Overview', route: 'overview' },
+		{ icon: 'folder', text: 'Projects', route: 'projects' },
+		{ icon: 'chat', text: 'Chat', route: 'chat' },
+		{
+			icon: 'settings',
+			text: 'Settings',
+			route: 'settings',
+			styles: 'auto',
+		},
+		{ icon: 'logout', text: 'Log out', route: 'logout' },
+	];
 
 	constructor() {}
 	ngOnInit(): void {}
