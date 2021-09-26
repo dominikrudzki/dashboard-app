7824bfe520646e20e6db1f98cc2d2c1823ca67d7
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-09-26 22:17:41 +0200

    add userData getter

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { CookieService } from './cookie.service';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class DataService {
-	private userData: any;
+	private userData!: { username: string };
 
 	constructor(private CookieService: CookieService) {}
 
 	setUserData(userData: any) {
 		this.userData = userData;
 		this.CookieService.setCookie('user', this.userData.username);
+	}
+
+	getUserData() {
+		return this.userData;
 	}
 
 	logIn(value: { username: string; password: string }) {}
