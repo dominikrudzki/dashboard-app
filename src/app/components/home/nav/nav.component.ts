6057d0fea9e0f222bf9e73a00cb0df8111c93707
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-09-26 22:19:47 +0200

    add username display in nav

--- src/app/components/home/nav/nav.component.ts
+++ src/app/components/home/nav/nav.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'src/app/services/cookie.service';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+	selector: 'app-nav',
+	templateUrl: './nav.component.html',
+	styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+	username: string;
 
-  constructor() { }
+	constructor(private CookieService: CookieService) {
+		this.username = this.CookieService.cookieUserName;
+	}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
