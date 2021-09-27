77a1521c6690ac16fd2f93470bd5488ab4367c52
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-09-27 17:15:20 +0200

    add fetching todos from firebase

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Todos } from '../shared/interfaces';
 import { CookieService } from './cookie.service';
 
 @Injectable({
@@ -7,8 +8,12 @@ import { CookieService } from './cookie.service';
 })
 export class DataService {
 	private userData!: { username: string };
+	private todos: Todos | any = { todo: [], inProgress: [], done: [] };
 
-	constructor(private CookieService: CookieService) {}
+	constructor(
+		private CookieService: CookieService,
+		private firestore: AngularFirestore
+	) {}
 
 	setUserData(userData: any) {
 		this.userData = userData;
@@ -22,4 +27,39 @@ export class DataService {
 	logIn(value: { username: string; password: string }) {}
 
 	register(value: { username: string; password: string }) {}
+
+	clearTodos() {
+		for (let key in this.todos) {
+			this.todos[key] = [];
+		}
+	}
+
+	fetchTodos() {
+		console.log('fetchTodos!');
+		this.clearTodos();
+		// const user = this.cookieService.getCookieValue('user');
+		this.todos;
+
+		const user = 'admin';
+
+		try {
+			this.firestore
+				.collection(`users/${user}/todo`)
+				.valueChanges()
+				.subscribe((data: any) => {
+					data.forEach((item: any) => {
+						if (item.list === 'todo') this.todos.todo.push(item);
+						if (item.list === 'inProgress')
+							this.todos.inProgress.push(item);
+						if (item.list === 'done') this.todos.done.push(item);
+					});
+				});
+		} catch {
+			// this.openSnackBar('Fill in the fields');
+		}
+	}
+
+	getTodos() {
+		return this.todos;
+	}
 }
