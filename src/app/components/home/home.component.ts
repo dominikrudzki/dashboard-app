293fab62789fb5c28908d683fb7a5782e6212681
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-09-27 17:15:50 +0200

    add fetch todos  on login

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,6 +14,7 @@ export class HomeComponent implements OnInit {
 		private DataService: DataService
 	) {
 		this.CookieService.checkCookie();
+		this.DataService.fetchTodos();
 	}
 
 	ngOnInit(): void {
