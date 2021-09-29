4391ea01d06032f6e7883bb69b9f67a26b6a7730
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-09-29 17:31:10 +0200

    fix username in chat

--- src/app/components/home/chat/chat.component.ts
+++ src/app/components/home/chat/chat.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'src/app/services/cookie.service';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+	selector: 'app-chat',
+	templateUrl: './chat.component.html',
+	styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
+	username: string;
 
-  constructor() { }
+	constructor(private CookieService: CookieService) {
+		this.username = this.CookieService.cookieUserName;
+	}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
