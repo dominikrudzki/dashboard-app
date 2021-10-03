b1d5bbd63a3be3dd6d59b4c57ddde5c231267fb6
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-10-03 18:37:41 +0200

    create ChatComponent logic

--- src/app/components/home/chat/chat.component.ts
+++ src/app/components/home/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { CookieService } from 'src/app/services/cookie.service';
 
 @Component({
 	selector: 'app-chat',
@@ -8,8 +9,12 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class ChatComponent implements OnInit {
 	messages: any;
+	message: string = '';
 
-	constructor(private firestore: AngularFirestore) {
+	constructor(
+		private firestore: AngularFirestore,
+		private CookieService: CookieService
+	) {
 		this.messages = [
 			{ author: 'teste', date: new Date(), message: 'test!' },
 		];
@@ -17,17 +22,37 @@ export class ChatComponent implements OnInit {
 
 	ngOnInit() {
 		// prepare for live updates
-
-		const chatDB = this.firestore.collection('chat').doc('messages');
-
-		const sub = chatDB.valueChanges().subscribe((messages: any) => {
-			console.log(messages.all[0]);
-
-			this.messages = messages.all;
-		});
 		// .snapshotChanges()
 		// .subscribe((doc) => {
 		// 	console.log('change', doc);
 		// });
+
+		const sub = this.firestore
+			.collection('chat')
+			.valueChanges()
+			.subscribe((messages: any) => {
+				console.log(messages);
+
+				this.messages = messages;
+			});
+	}
+
+	async sendMessage() {
+		console.log(this.message);
+
+		await this.firestore.collection('chat').add({
+			id: new Date().valueOf(),
+			author: this.CookieService.getCookieValue('user'),
+			message: this.message,
+			date: new Date(),
+		});
+
+		this.message = '';
+
+		// this.firestore
+		// 	.collection('chat')
+		// 	.doc('messages/all')
+		// 	.set({ name: 'bbbb' }, { merge: true });
+		console.log('submit');
 	}
 }
