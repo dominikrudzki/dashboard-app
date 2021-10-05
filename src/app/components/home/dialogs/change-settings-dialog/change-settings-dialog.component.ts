ab76ca5dc62bd78787dd44f985138c5565aeba1e
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-10-05 16:28:48 +0200

    add initial ChangeSettingsDialogComponent logic

--- src/app/components/home/dialogs/change-settings-dialog/change-settings-dialog.component.ts
+++ src/app/components/home/dialogs/change-settings-dialog/change-settings-dialog.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
-  selector: 'app-change-settings-dialog',
-  templateUrl: './change-settings-dialog.component.html',
-  styleUrls: ['./change-settings-dialog.component.scss']
+	selector: 'app-change-settings-dialog',
+	templateUrl: './change-settings-dialog.component.html',
+	styleUrls: ['./change-settings-dialog.component.scss'],
 })
 export class ChangeSettingsDialogComponent implements OnInit {
+	input: string = '';
 
-  constructor() { }
+	constructor(
+		public dialogRef: MatDialogRef<ChangeSettingsDialogComponent>,
+		@Inject(MAT_DIALOG_DATA) public data: any,
+		private DataService: DataService
+	) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	changeValue() {
+		switch (this.data.title) {
+			case 'avatar':
+				console.log('avatar change');
+				this.DataService.setUserAvatar(this.input);
+				console.log(this.DataService.getUserData());
+
+				break;
+			case 'password':
+				console.log('password change');
+
+				break;
+		}
+	}
 }
