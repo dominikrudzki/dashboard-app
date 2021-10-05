bc41e5001c8adaddb11914e38bdd7b6bbb28547d
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-10-05 16:29:19 +0200

    add SettingsComponent logic

--- src/app/components/home/settings/settings.component.ts
+++ src/app/components/home/settings/settings.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DataService } from 'src/app/services/data.service';
+import { ChangeSettingsDialogComponent } from '../dialogs/change-settings-dialog/change-settings-dialog.component';
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+	selector: 'app-settings',
+	templateUrl: './settings.component.html',
+	styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+	user;
 
-  constructor() { }
+	constructor(private DataService: DataService, public dialog: MatDialog) {
+		this.user = DataService.getUserData();
+		console.log(this.user);
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	openSettingsDialog(
+		list: string,
+		dialogTitle: string,
+		dialogPlaceholder: string
+	) {
+		const dialogRef = this.dialog.open(ChangeSettingsDialogComponent, {
+			width: '350px',
+			data: { title: dialogTitle, placeholder: dialogPlaceholder },
+		});
+
+		dialogRef.afterClosed().subscribe((result) => {
+			console.log('The dialog was closed');
+		});
+	}
 }
