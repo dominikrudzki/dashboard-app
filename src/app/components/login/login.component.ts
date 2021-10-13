9c08579fb20f87a54a33686b446c794dc9c30045
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-10-13 20:48:41 +0200

    add initial form validate on login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { CookieService } from 'src/app/services/cookie.service';
@@ -41,32 +41,44 @@ export class LoginComponent implements OnInit {
 
 	reactiveForm() {
 		this.loginForm = this.formBuilder.group({
-			username: [''],
-			password: [''],
+			username: ['', Validators.required],
+			password: ['', Validators.required],
 		});
+
+
 	}
 
-	submitForm(value: any) {
-		try {
-			this.firestore
-				.collection('users')
-				.doc(`${value.username}`)
-				.valueChanges()
-				.subscribe((data: { password: string } | any) => {
-					if (data.password === value.password) {
-						console.log('You are logged in!');
-						this.DataService.setUserData({
-							avatar: '',
-							username: value.username,
-						});
-
-						this.route.navigate(['/home']);
-					} else {
-						this.openSnackBar('Wrong password');
-					}
-				});
-		} catch {
+	async submitForm(value: any) {
+		if (this.loginForm.invalid) {
 			this.openSnackBar('Fill in the fields');
+			return
+		}
+
+		try {
+			const snapshot = await this.firestore
+				.collection('users').ref.where('username', '==' , value.username).get()
+
+			if (snapshot.empty) {
+				this.openSnackBar('Wrong username or password');
+			}
+
+				// .doc(`${value.username}`)
+				// .valueChanges()
+				// .subscribe((data: { password: string } | any) => {
+				// 	if (data.password === value.password) {
+				// 		console.log('You are logged in!');
+				// 		this.DataService.setUserData({
+				// 			avatar: '',
+				// 			username: value.username,
+				// 		});
+
+				// 		this.route.navigate(['/home']);
+				// 	} else {
+				// 		this.openSnackBar('Wrong password');
+				// 	}
+				// });
+		} catch {
+
 		}
 	}
 }
