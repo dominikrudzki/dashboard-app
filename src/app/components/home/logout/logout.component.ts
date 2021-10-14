dcd174b634a3e5c1b19298c9aa7615ee1df82f32
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-10-14 21:17:54 +0200

    add logout user data clear

--- src/app/components/home/logout/logout.component.ts
+++ src/app/components/home/logout/logout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'src/app/services/cookie.service';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
 	selector: 'app-logout',
@@ -7,9 +8,17 @@ import { CookieService } from 'src/app/services/cookie.service';
 	styleUrls: ['./logout.component.scss'],
 })
 export class LogoutComponent implements OnInit {
-	constructor(private cookieService: CookieService) {}
+	constructor(
+		private cookieService: CookieService,
+		private DataService: DataService
+	) {}
 
 	ngOnInit(): void {
 		this.cookieService.deleteAllCookies();
+		this.DataService.userData = {
+			avatar: '',
+			username: '',
+			createDate: 1,
+		};
 	}
 }
